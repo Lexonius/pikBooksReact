5bbe710e674b9785ca07bc9f53d633c637756749
Сырцов Алексей Алексеевич <syrtsovaa@pik.ru>
2019-03-05 19:36:16 +0300

    modal window changed v2

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import React, { Component } from 'react';
-import axios from 'axios'
-import './App.css';
-import { Input } from 'antd';
-import { Button } from 'antd';
-import { Table } from 'antd';
-import ModalWindow from './components/ModalWindow'
-import AuthWindow from './components/AuthWindow'
+import React, { Component } from "react";
+import axios from "axios";
+import "./App.css";
+import { Input } from "antd";
+import { Button } from "antd";
+import { Table } from "antd";
+import ModalWindow from "./components/ModalWindow";
+import AuthWindow from "./components/AuthWindow";
 // import { requestAddressValid } from "./config";
 import { requestAddress } from "./config";
 // import { requestAddressValidText } from "./config";
@@ -28,68 +28,8 @@ class App extends Component {
         pages: undefined,
         year: undefined
       },
-      modalData:{
-        displayModalWindow: 'none'
-      }
+      modalData: "none"
     };
-  }
-  // state = {
-  //   data: [],
-  // }
-
-  render() {
-    const columns = [{
-      title: 'Name',
-      dataIndex:'name',
-      width: 150,
-      key:'name',
-      sorter: (a, b) => a.name.length - b.name.length,
-      sortDirections: ['descend'],
-    }, {
-      title: 'Author',
-      dataIndex:'author',
-      width: 150,
-      key:'author',
-      sorter: (a, b) => a.author.length - b.author.length,
-      sortDirections: ['descend'],
-    }]
-    console.log(this.state);
-    console.log(this.props);
-    
-    return (
-      <div className="App" >
-       
-          <div className='tableWrap'>
-            <div className="top-table">
-              <Button> Add Book </Button>
-              <Input placeholder="Basic usage" onChange = {this.searchBooks} className="searchInput"/>        
-            </div>
-            <div>
-              <Table 
-                onRow={(record, rowIndex) => {
-                  return {
-                    onClick: (event) => {
-                      // event.persist()
-                      // event.preventDefault()
-                      console.log(record);
-                      // this.setState({chooseBook: record})
-                      // console.log(this.state);
-                      // this.props = record
-                      console.log(this.props);
-                      this.setState({chooseBook: record});
-                      console.log(this.state.chooseBook.id);
-                      this.setState({modalData:'block'})
-                    },       
-                  };
-                }}
-              
-              columns={columns} dataSource={this.state.data} size="small" />
-              <ModalWindow chooseBook={this.state.chooseBook} style={this.state.modalData}/> 
-            </div>
-        </div>
-      </div>
-
-    );
   }
 
   async getFilterBooks(filterValue) {
@@ -113,10 +53,10 @@ class App extends Component {
       ],
       fields: "id,name,author,year,country,language,pages,link,pic",
       pageSize: 150,
-      allObjects: true,
+      allObjects: true
     });
     // this.paginationAllBooks === false;
-    let filteredBooks = []
+    let filteredBooks = [];
     searchRequest.data.result.list.forEach(element => {
       let foundBooks = {
         id: element.obj.id,
@@ -130,20 +70,21 @@ class App extends Component {
         link: element.obj.link,
         key: element.obj.id
       };
-      filteredBooks.push(foundBooks)
+      filteredBooks.push(foundBooks);
     });
-    this.setState({data: filteredBooks})
-    console.log('filter',this.state.data);
+    this.setState({ data: filteredBooks });
+    console.log("filter", this.state.data);
   }
 
-  searchBooks = (event) => {
-    event.persist()
-    if(this.state.filterInputValue === ""){
-      this.getAllBooks(event.target.value)
+  searchBooks = event => {
+    // event.persist()
+    this.setState({ modalData: "none" });
+    if (this.state.filterInputValue === "") {
+      this.getAllBooks(event.target.value);
     } else {
-      this.getFilterBooks(event.target.value)
+      this.getFilterBooks(event.target.value);
     }
-  }
+  };
 
   async getAllBooks() {
     const request = await axios.post(requestAddress, {
@@ -171,16 +112,87 @@ class App extends Component {
         year: element.obj.year,
         avatar: element.obj.pic,
         link: element.obj.link,
-        key: element.obj.id,
+        key: element.obj.id
       };
-      booksArr.push(booksField)
+      booksArr.push(booksField);
     });
-    this.setState({data: booksArr})
-    console.log('all',this.state.data);  
+    this.setState({ data: booksArr });
+    console.log("all", this.state.data);
   }
-  
+
   componentWillMount() {
-    this.getAllBooks() 
+    this.getAllBooks();
+  }
+
+  onAddBook = e => {
+    this.setState({ chooseBook: {} });
+    this.setState({ modalData: "inline" });
+  };
+
+  render() {
+    const columns = [
+      {
+        title: "Name",
+        dataIndex: "name",
+        width: 150,
+        key: "name",
+        sorter: (a, b) => a.name.length - b.name.length,
+        sortDirections: ["descend"]
+      },
+      {
+        title: "Author",
+        dataIndex: "author",
+        width: 150,
+        key: "author",
+        sorter: (a, b) => a.author.length - b.author.length,
+        sortDirections: ["descend"]
+      }
+    ];
+    console.log(this.state);
+    console.log(this.props);
+
+    return (
+      <div className="App">
+        <div className="tableWrap">
+          <div className="top-table">
+            <Button onClick={this.onAddBook}> Add Book </Button>
+            <Input
+              placeholder="Basic usage"
+              onChange={this.searchBooks}
+              className="searchInput"
+            />
+          </div>
+          <div>
+            <Table
+              onRow={(record, rowIndex) => {
+                return {
+                  onClick: event => {
+                    // event.persist()
+                    // event.preventDefault()
+                    console.log(record);
+                    // this.setState({chooseBook: record})
+                    // console.log(this.state);
+                    // this.props = record
+                    console.log(this.props);
+                    this.setState({ chooseBook: record });
+                    console.log(this.state.chooseBook.id);
+                    this.setState({ modalData: "block" });
+                  }
+                };
+              }}
+              columns={columns}
+              dataSource={this.state.data}
+              size="small"
+            />
+            <ModalWindow
+              chooseBook={this.state.chooseBook}
+              style={this.state.modalData}
+              fields={this.state.disabledFields}
+            />
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 
--- src/components/ModalWindow.js
+++ src/components/ModalWindow.js
@@ -1,170 +1,240 @@
-import React, { Component } from 'react';
-import axios from 'axios'
-import '../App.css';
-import { Input } from 'antd';
-import { Button } from 'antd';
-import { Icon } from 'antd';
-import { requestAddressValid } from '../config';
+import React, { Component } from "react";
+import axios from "axios";
+import "../App.css";
+import { Input } from "antd";
+import { Button } from "antd";
+import { Icon } from "antd";
+import { requestAddressValid } from "../config";
 // import { requestAddressValid } from "./config";
 // import { requestAddress } from "./config";
 // import { requestAddressValidText } from "./config";
 // import { getUserAddress } from "./config";
 
-class ModalWindow extends Component{
+class ModalWindow extends Component {
   constructor(props) {
     super(props);
     this.state = {
-        id: "",
-        author:"",
-        country: "",
-        language: "",
-        name: "",
-        pages: "",
-        year: "",
-        avatar: "",
-        link: "",
-        key:"",        
-        displayModalWindow:"none",
-        defaultBookCover: 'http://volkhonkamansion.ru/wp-content/uploads/2016/05/ikonka-knig.jpg'
+      id: "",
+      author: "",
+      country: "",
+      language: "",
+      name: "",
+      pages: "",
+      year: "",
+      avatar: "",
+      link: "",
+      key: "",
+      displayModalWindow: "none",
+      defaultBookCover:
+        "http://volkhonkamansion.ru/wp-content/uploads/2016/05/ikonka-knig.jpg",
+      disableFields: true
     };
-    this.changeBookField = this.changeBookField.bind(this);
   }
 
-  render(){
-    console.log(this.state);
-    console.log(this.props);  
-    let display = this.props
-    
-    return (
-      <div className='wrapper' style={{display: this.state.displayModalWindow}}>
-        <div className="top">
-        <div className='headerText'>"{this.props.chooseBook.name}"" {this.props.chooseBook.author}</div>
-        <Icon type="close" onClick={this.closeModalWindow}/>
-        </div>
-        <div className='body'>
-          <div className='left'>
-            <form className="inputsBlock">
-              <div className="input">
-                <Input name="id" placeholder={this.props.chooseBook.id}  onChange = {this.changeBookFieldNum} value={this.state.id}className="searchInput" />      
-              </div>
-              <div className="input">
-                <Input name = "name" placeholder={this.props.chooseBook.name} type='text' onChange = {this.changeBookField.bind(this, 'chooseBook')} value={this.state.name} className="searchInput" />      
-              </div>
-              <div className="input">
-                <Input name = "author" placeholder={this.props.chooseBook.author} onChange = {this.changeBookField} value={this.state.author} className="searchInput" />      
-              </div>
-              <div className="input">
-                <Input name="year" placeholder={this.props.chooseBook.year} onChange = {this.changeBookFieldNum} value={this.state.year} className="searchInput" />      
-              </div>
-              <div className="input">
-                <Input name = "country" placeholder={this.props.chooseBook.country} onChange = {this.changeBookField} value={this.state.country} className="searchInput" />      
-              </div>
-              <div className="input">
-                <Input name = "language" placeholder={this.props.chooseBook.language} onChange = {this.changeBookField} value={this.state.language} className="searchInput" />      
-              </div>
-              <div className="input">
-                <Input name="pages" placeholder={this.props.chooseBook.pages} onChange = {this.changeBookFieldNum} value={this.state.pages} className="searchInput" />      
-              </div>         
-            </form>     
-          </div>
-          <div className='right'>
-            <img className='avatar' src={this.state.avatar === false ? this.state.defaultBookCover : this.state.avatar} name="avatar"></img>
-            <div className="input">
-             <Input name="avatar" placeholder={this.props.chooseBook.avatar} onChange = {this.changeBookField} className="searchInput" />     
-            </div>
-            <div className="input">
-              <Input name="link" placeholder={this.props.chooseBook.link} onChange = {this.changeBookField} value={this.state.link} className="searchInput" />       
-            </div>
-          </div> 
-        </div>
-        <div className="buttonBlock">
-          <Button type="primary"  onClick={this.getThisBook}>Get this book</Button>
-          <Button >Change</Button>
-          <Button  onClick={this.saveObj}>Save</Button>
-
-          <Button type="danger">Delete</Button>
-        </div> 
-      </div>
-    );
-  }
-
-  // changeBookFieldNum = (e) => {
-  //   e.persist()
-  //   console.log(e.target.name,e.target.value);
-    
-  //   if(isNaN(e.target.value) || e.target.value === ""){
-  //     console.log('Error'); 
-  //     return
-  //   } 
-  //   this.setState({[e.target.name]: e.target.value})
-  //   console.log(this.state);
-  // } 
-
-  // changeAvatar = (e) => {
-  //   this.setState({avatar:e.target.value})
-  //   console.log(this.state);
-    
-  // }
-
-  getThisBook = (e) => {
-    window.location = this.state.link;
-  }
-
-  closeModalWindow = (e) => {
-    console.log(this.display);
-    this.setState({displayModalWindow: 'none'})
-  }
-
-  changeBookFieldNum = (e) => {
-    this.setState({[e.target.name]: e.target.value})
-  }
-
-  changeBookField = (e) => {
-    this.setState({[e.target.name]: e.target.value})
-  }
-
-  saveObj = async () => {
-    console.log(this.state);
-      let updateReq = await axios.post(requestAddressValid,{
-        author: this.state.author,
-        country: this.state.country,
-        id: this.state.id,
-        language: this.state.language,
-        name: this.state.name,
-        pages: this.state.pages,
-        year: this.state.year,
-        bookAvatar: this.state.avatar,
-        link: this.state.link,
-        isDelete: false,
-        isComplete: ""
-      })
-      console.log(updateReq);
-  }
-
-  componentWillUpdate(prevProps, prevState){
+  componentWillUpdate(prevProps, prevState) {
     console.log(prevProps);
     console.log(prevState);
     console.log(this.state);
-    this.state.avatar = prevProps.chooseBook.avatar
-    this.state.id = prevProps.chooseBook.id
-    this.state.name = prevProps.chooseBook.name
-    this.state.author = prevProps.chooseBook.author
-    this.state.country = prevProps.chooseBook.country
-    this.state.language = prevProps.chooseBook.language
-    this.state.pages = prevProps.chooseBook.pages
-    this.state.year = prevProps.chooseBook.year
-    this.state.link = prevProps.chooseBook.link
-    this.state.key = prevProps.chooseBook.key
-    this.state.displayModalWindow = prevProps.style
-    if(prevProps.chooseBook.avatar === ""){
-      this.state.avatar = false
+    this.state.avatar = prevProps.chooseBook.avatar;
+    this.state.id = prevProps.chooseBook.id;
+    this.state.name = prevProps.chooseBook.name;
+    this.state.author = prevProps.chooseBook.author;
+    this.state.country = prevProps.chooseBook.country;
+    this.state.language = prevProps.chooseBook.language;
+    this.state.pages = prevProps.chooseBook.pages;
+    this.state.year = prevProps.chooseBook.year;
+    this.state.link = prevProps.chooseBook.link;
+    this.state.key = prevProps.chooseBook.key;
+    this.state.displayModalWindow = prevProps.style;
+    if (prevProps.chooseBook.avatar === "") {
+      this.state.avatar = false;
     }
-  } 
+  }
 
-  componentDidUpdate(prevProps, prevState){
+  getThisBook = e => {
+    window.location = this.state.link;
+  };
 
+  deleteBook = async () => {
+    let deletedReq = await axios.post(requestAddressValid, {
+      id: this.state.id,
+      isDelete: true
+    });
+    console.log(deletedReq);
+  };
+
+  closeModalWindow = e => {
+    console.log(this.display);
+    this.setState({ disableFields: true });
+    this.setState({ displayModalWindow: "none" });
+  };
+
+  changeBookFieldNum = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
+  changeBookField = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
+  changeFields = e => {
+    console.log("45");
+    this.setState({ disableFields: false });
+  };
+
+  saveObj = async () => {
+    console.log(this.state);
+    let updateReq = await axios.post(requestAddressValid, {
+      author: this.state.author,
+      country: this.state.country,
+      id: this.state.id,
+      language: this.state.language,
+      name: this.state.name,
+      pages: this.state.pages,
+      year: this.state.year,
+      bookAvatar: this.state.avatar,
+      link: this.state.link,
+      isDelete: false,
+      isComplete: ""
+    });
+    console.log(updateReq);
+  };
+
+  render() {
+    console.log(this.state);
+    console.log(this.props);
+    return (
+      <div
+        className="wrapper"
+        style={{ display: this.state.displayModalWindow }}
+      >
+        <div className="top">
+          <div className="headerText">
+            "{this.props.chooseBook.name}" {this.props.chooseBook.author}
+          </div>
+          <Icon type="close" onClick={this.closeModalWindow} />
+        </div>
+        <div className="body">
+          <div className="left">
+            <form className="inputsBlock">
+              <div className="input">
+                <Input
+                  name="id"
+                  placeholder={this.props.chooseBook.id}
+                  onChange={this.changeBookFieldNum}
+                  value={this.state.id}
+                  className="searchInput"
+                  disabled={this.state.disableFields}
+                />
+              </div>
+              <div className="input">
+                <Input
+                  name="name"
+                  placeholder={this.props.chooseBook.name}
+                  type="text"
+                  onChange={this.changeBookField.bind(this, "chooseBook")}
+                  value={this.state.name}
+                  className="searchInput"
+                  disabled={this.state.disableFields}
+                />
+              </div>
+              <div className="input" disabled={true}>
+                <Input
+                  name="author"
+                  placeholder={this.props.chooseBook.author}
+                  onChange={this.changeBookField}
+                  value={this.state.author}
+                  className="searchInput"
+                  disabled={this.state.disableFields}
+                />
+              </div>
+              <div className="input">
+                <Input
+                  name="year"
+                  placeholder={this.props.chooseBook.year}
+                  onChange={this.changeBookFieldNum}
+                  value={this.state.year}
+                  className="searchInput"
+                  disabled={this.state.disableFields}
+                />
+              </div>
+              <div className="input">
+                <Input
+                  name="country"
+                  placeholder={this.props.chooseBook.country}
+                  onChange={this.changeBookField}
+                  value={this.state.country}
+                  className="searchInput"
+                  disabled={this.state.disableFields}
+                />
+              </div>
+              <div className="input">
+                <Input
+                  name="language"
+                  placeholder={this.props.chooseBook.language}
+                  onChange={this.changeBookField}
+                  value={this.state.language}
+                  className="searchInput"
+                  disabled={this.state.disableFields}
+                />
+              </div>
+              <div className="input">
+                <Input
+                  name="pages"
+                  placeholder={this.props.chooseBook.pages}
+                  onChange={this.changeBookFieldNum}
+                  value={this.state.pages}
+                  className="searchInput"
+                  disabled={this.state.disableFields}
+                />
+              </div>
+            </form>
+          </div>
+          <div className="right">
+            <img
+              className="avatar"
+              src={
+                this.state.avatar === false
+                  ? this.state.defaultBookCover
+                  : this.state.avatar
+              }
+              name="avatar"
+            />
+            <div className="input">
+              <Input
+                name="avatar"
+                placeholder={this.props.chooseBook.avatar}
+                onChange={this.changeBookField}
+                className="searchInput"
+                disabled={this.state.disableFields}
+              />
+            </div>
+            <div className="input">
+              <Input
+                name="link"
+                placeholder={this.props.chooseBook.link}
+                onChange={this.changeBookField}
+                value={this.state.link}
+                className="searchInput"
+                disabled={this.state.disableFields}
+              />
+            </div>
+          </div>
+        </div>
+        <div className="buttonBlock">
+          <Button type="primary" onClick={this.getThisBook}>
+            Get this book
+          </Button>
+          <Button onClick={this.changeFields}>Change</Button>
+          <Button onClick={this.saveObj}>Save</Button>
+
+          <Button type="danger" onClick={this.deleteBook}>
+            Delete
+          </Button>
+        </div>
+      </div>
+    );
   }
 }
 
-
-export default ModalWindow
+export default ModalWindow;
